3fb3c82b1c85d5f25dfeaf5579456b4f3f6612ef
Sebastian Murillo <sebastianmurillonader@gmail.com>
2020-01-03 14:11:11 -0600

    Test chamges

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ render={data => (
                     {product.price}€
                   </div>
                 </div>
-                <span className="Product__buy">Buy now</span>
+                <span className="Product__buy">Buy nooow</span>
               </div>
             </div>
           </div>

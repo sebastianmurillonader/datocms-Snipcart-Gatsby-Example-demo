aafe607f097aa298cca880371a7d876094c63eb4
Sebastian Murillo <sebastianmurillonader@gmail.com>
2020-01-04 13:52:44 -0600

    Add react packages and helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,9 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: `gatsby-source-datocms`,
       options: { apiToken: process.env.DATO_API_TOKEN },
@@ -19,6 +22,20 @@ module.exports = {
         apiKey: 'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
         autopop: true
       }
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: path.resolve(__dirname, 'src/assets/svg'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-layout',
+      options: {
+        component: require.resolve('./src/components/app-layout/AppLayout.tsx'),
+      },
     },
   ],
 }
